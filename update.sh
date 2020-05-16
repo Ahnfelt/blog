0c5600e87e44d74891859a5c686e6a0f59fee169
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2020-05-16 08:56:18 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@
 
 # Blog directory
 # mkdir www
+# mkdir hakyll
 # mkdir blog
 
 # Get caddy
@@ -15,16 +16,19 @@
 # Get stack
 # wget -qO- https://get.haskellstack.org/ | sh
 
+# Get Hakyll (in hakyll)
+# stack install hakyll && stack exec hakyll-init site
+
 # Get screen
 # apt install screen
 
 # Clone the repository
 # git clone https://github.com/Ahnfelt/blog.git
 
-# Make a directory for caddy logs
+# Make a directory for caddy logs (and enable logging here in the Caddyfile)
 # mkdir /var/log/caddy && chown caddy:caddy /var/log/caddy && chmod o-rwx /var/log/caddy
 
-# Make a script that runs in a loop, greps for the hook, if it changed:
+# Make a script that runs in a loop, greps the log for the hook. If it changed:
 # (cd ../../blog && git pull && bash update.sh)
 
 
@@ -32,3 +36,10 @@
 # Update script
 #################
 
+(
+rm -rf ../hakyll &&
+cp -r ../blog ../hakyll &&
+cd ../hakyll &&
+stack build && 
+stack exec site build
+)
