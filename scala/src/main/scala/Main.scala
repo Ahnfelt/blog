6ebd9e9faf45d7e422fdf5821a4c77b0367a41bf
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2020-05-24 18:38:39 +0200

    Styling

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -5,9 +5,13 @@ object Main {
 
     def main(arguments : Array[String]) : Unit = {
 
+        val templatesFile = new File(arguments(1))
+        val authorTemplateFile = new File(templatesFile, "post-author.html")
+        val authorTemplate = new String(Files.readAllBytes(authorTemplateFile.toPath), "UTF-8")
+
         val postsFile = new File(arguments(0))
 
-        val articleProcessor = new ArticleProcessor("<div>XXXXXXXXX</div>")
+        val articleProcessor = new ArticleProcessor(authorTemplate)
         assert(postsFile.exists())
         for(postFile <- postsFile.listFiles().toList if postFile.isDirectory) {
             val articleFile = new File(postFile, "article.md")
