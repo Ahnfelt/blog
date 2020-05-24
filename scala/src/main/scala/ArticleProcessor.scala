39b7f23ea9d807fce4fffbe57bac2a5207df9fb8
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2020-05-24 13:02:41 +0200

    Styling

---
+++ scala/src/main/scala/ArticleProcessor.scala
@@ -0,0 +1,83 @@
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
+import java.util
+
+import com.vladsch.flexmark.ast.{Heading, Image, Paragraph}
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
+import com.vladsch.flexmark.ext.tables.TablesExtension
+import com.vladsch.flexmark.ext.typographic.TypographicExtension
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.ast.{Document, TextCollectingVisitor}
+import com.vladsch.flexmark.util.data.MutableDataSet
+import com.vladsch.flexmark.util.misc.Extension
+
+import scala.util.matching.Regex
+
+class ArticleProcessor(authorHtmlTemplate : String) {
+
+    private val options = new MutableDataSet()
+
+    options.set(Parser.EXTENSIONS, util.Arrays.asList[Extension](
+        TablesExtension.create(),
+        StrikethroughExtension.create(),
+        TypographicExtension.create(),
+    ))
+
+    options.set(HtmlRenderer.GENERATE_HEADER_ID, java.lang.Boolean.TRUE)
+    options.set(HtmlRenderer.RENDER_HEADER_ID, java.lang.Boolean.TRUE)
+    options.set(HtmlRenderer.HEADER_ID_GENERATOR_NO_DUPED_DASHES, java.lang.Boolean.TRUE)
+
+    private val parser = Parser.builder(options).build()
+    private val renderer = HtmlRenderer.builder(options).build()
+
+    def process(path : Path) : ArticleInfo = {
+        val markdown = new String(Files.readAllBytes(path), "UTF-8")
+        val document = parser.parse(markdown)
+        val (title, image, teaser) = extractMeta(document)
+        val documentHtml = renderer.render(document)
+        val authorHtml = authorHtmlTemplate
+        val html = documentHtml.replaceFirst("</h1>", "</h1>\n" + Regex.quoteReplacement(authorHtml))
+        ArticleInfo(
+            articleHtml = html,
+            title = Some(title.trim).filter(_.nonEmpty),
+            imageUrl = Some(image.trim).filter(_.nonEmpty),
+            teaser = Some(teaser.trim).filter(_.nonEmpty),
+        )
+    }
+
+    private def extractMeta(document : Document) : (String, String, String) = {
+        val visitor = new TextCollectingVisitor()
+        document.getFirstChild match {
+            case heading : Heading =>
+                val (imageText, imageParagraph) = heading.getNext match {
+                    case paragraph : Paragraph =>
+                        paragraph.getFirstChild match {
+                            case image : Image =>
+                                image.getUrl.toString -> Some(paragraph)
+                            case _ =>
+                                "" -> None
+                        }
+                    case _ =>
+                        "" -> None
+                }
+                val teaserText = imageParagraph.getOrElse(heading).getNext match {
+                    case paragraph : Paragraph =>
+                        visitor.collectAndGetText(paragraph)
+                    case _ =>
+                        ""
+                }
+                (visitor.collectAndGetText(heading), imageText, teaserText)
+            case _ =>
+                ("", "", "")
+        }
+    }
+
+}
+
+case class ArticleInfo(
+    articleHtml : String,
+    title : Option[String],
+    imageUrl : Option[String],
+    teaser : Option[String]
+)
--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -1,75 +1,21 @@
-import java.util
-
-import com.vladsch.flexmark.ast.{Heading, HtmlBlock, Image, Paragraph}
-import com.vladsch.flexmark.ext.anchorlink.AnchorLinkExtension
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
-import com.vladsch.flexmark.ext.tables.TablesExtension
-import com.vladsch.flexmark.ext.typographic.TypographicExtension
-import com.vladsch.flexmark.html.HtmlRenderer
-import com.vladsch.flexmark.parser.Parser
-import com.vladsch.flexmark.parser.block.{NodePostProcessor, NodePostProcessorFactory}
-import com.vladsch.flexmark.util.ast.{Document, Node, NodeTracker, TextCollectingVisitor}
-import com.vladsch.flexmark.util.data.MutableDataSet
-import com.vladsch.flexmark.util.misc.Extension
-import com.vladsch.flexmark.util.sequence.BasedSequence
-
-import scala.util.matching.Regex
+import java.io.File
+import java.nio.file._
 
 object Main {
 
     def main(arguments : Array[String]) : Unit = {
 
-        val options = new MutableDataSet()
+        val postsFile = new File(arguments(0))
 
-        options.set(Parser.EXTENSIONS, util.Arrays.asList[Extension](
-            TablesExtension.create(),
-            StrikethroughExtension.create(),
-            TypographicExtension.create(),
-        ))
-
-        options.set(HtmlRenderer.GENERATE_HEADER_ID, java.lang.Boolean.TRUE)
-        options.set(HtmlRenderer.RENDER_HEADER_ID, java.lang.Boolean.TRUE)
-        options.set(HtmlRenderer.HEADER_ID_GENERATOR_NO_DUPED_DASHES, java.lang.Boolean.TRUE)
-
-        val parser = Parser.builder(options).build()
-        val renderer = HtmlRenderer.builder(options).build()
-
-
-        val document = parser.parse("# foo *bar*\n\nThis is *Sparta*\n\n## Yahoo --- there\n\nHello")
-        val (title, image, teaser) = extractMeta(document)
-        println((title, image, teaser))
-        val documentHtml = renderer.render(document)
-        val authorHtml = "<div>XXXXXXXXX</div>"
-        val html = documentHtml.replaceFirst("</h1>", "</h1>\n" + Regex.quoteReplacement(authorHtml))
-        println(html)
-
-    }
-
-    def extractMeta(document : Document) : (String, String, String) = {
-        val visitor = new TextCollectingVisitor()
-        document.getFirstChild match {
-            case heading : Heading =>
-                val (imageText, imageParagraph) = heading.getNext match {
-                    case paragraph : Paragraph =>
-                        paragraph.getFirstChild match {
-                            case image : Image =>
-                                image.getUrlContent.toString -> Some(paragraph)
-                            case _ =>
-                                "" -> None
-                        }
-                    case _ =>
-                        "" -> None
-                }
-                val teaserText = imageParagraph.getOrElse(heading).getNext match {
-                    case paragraph : Paragraph =>
-                        visitor.collectAndGetText(paragraph)
-                    case _ =>
-                        ""
-                }
-                (visitor.collectAndGetText(heading), imageText, teaserText)
-            case _ =>
-                ("", "", "")
+        val articleProcessor = new ArticleProcessor("<div>XXXXXXXXX</div>")
+        assert(postsFile.exists())
+        for(postFile <- postsFile.listFiles().toList if postFile.isDirectory) {
+            val articleFile = new File(postFile, "article.md")
+            if(articleFile.exists()) {
+                val articleInfo = articleProcessor.process(articleFile.toPath)
+                println(articleInfo)
+            }
         }
-    }
 
+    }
 }
